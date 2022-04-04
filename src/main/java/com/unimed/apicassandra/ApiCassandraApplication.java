f4d8d912a654edd9f53f587f6e50533d23cc293f
jeffersonfreitas21 <jefferson.dev21@gmail.com>
2022-04-04 16:50:15 -0300

    First commit

---
+++ src/main/java/com/unimed/apicassandra/ApiCassandraApplication.java
@@ -0,0 +1,13 @@
+package com.unimed.apicassandra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCassandraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCassandraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unimed/apicassandra/ApiCassandraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unimed.apicassandra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCassandraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
